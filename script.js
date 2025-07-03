c18cd02870174a363723d003fbec406e399dbbbf
FuoriScala <riofortresse@gmail.com>
2025-07-03 16:10:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Replace with your actual SheetDB API endpoint (keep the quotes)
+// Replace with your actual SheetDB API endpoint
 const SHEETDB_API_URL = 'https://sheetdb.io/api/v1/m9769bwpvfaga';
 
 const bubble = document.getElementById('emotionBubble');
